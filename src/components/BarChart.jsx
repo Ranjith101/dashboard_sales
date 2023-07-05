e530b738fe259b620d960faf26cbbc59728f85d0
Ranjith101 <89287908+Ranjith101@users.noreply.github.com>
2023-07-05 11:24:28 +0530

    init

---
+++ src/components/BarChart.jsx
@@ -0,0 +1,58 @@
+import { useTheme } from "@mui/material";
+import { ResponsiveBar } from "@nivo/bar";
+import { tokens } from "../theme";
+import { mockBarData as data } from "../data/mockData";
+
+const BarChart = ({ isDashboard = false }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <ResponsiveBar
+      data={data}
+      theme={{
+        // ...theme configuration
+      }}
+      keys={["hot dog", "burger"]} // Only keep the desired values in keys
+      indexBy="country"
+      margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+      padding={0.3}
+      valueScale={{ type: "linear" }}
+      indexScale={{ type: "band", round: true }}
+      colors={{ scheme: "nivo" }}
+      defs={[
+        {
+          // ...pattern defs
+        },
+      ]}
+      borderColor={{
+        from: "color",
+        modifiers: [["darker", "1.6"]],
+      }}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        // ...bottom axis configuration
+      }}
+      axisLeft={{
+        // ...left axis configuration
+      }}
+      enableLabel={false}
+      labelSkipWidth={12}
+      labelSkipHeight={12}
+      labelTextColor={{
+        from: "color",
+        modifiers: [["darker", 1.6]],
+      }}
+      legends={[
+        // ...legends configuration
+      ]}
+      role="application"
+      barAriaLabel={function (e) {
+        return e.id + ": " + e.formattedValue + " in country: " + e.indexValue;
+      }}
+    />
+  );
+};
+
+export default BarChart;
